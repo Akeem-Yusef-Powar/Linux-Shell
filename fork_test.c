06990cedcec29d55e618dfd781072532ba8ad110
McCoderFace3000 <59894218+McCoderFace3000@users.noreply.github.com>
2020-02-26 20:02:44 -0500

    Update fork_test.c

--- fork_test.c
+++ fork_test.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h> 
 
 int main(){
+
+char *path = "/home/TU/tug31545/Sumtest";
 
 
 int pid = fork();
@@ -9,10 +14,14 @@ int pid = fork();
 if(pid == 0){
     printf("in the child, doing work\n");
     printf("process ID: %d\n",pid);
+	execl(path,"Sumtest",(char*)0);
+	printf("This shouldn't print");
 }
 else {
-wait();
-printf("Back in parent");
+printf("do this before wait\n");
+wait(NULL);
+printf("Back in parent\n");
+printf("%d\n", getpid());
 }
 
 return 0;

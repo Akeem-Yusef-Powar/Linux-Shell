fe4c85002c4916ce19b33b2738f55ae47424db9f
McCoderFace3000 <59894218+McCoderFace3000@users.noreply.github.com>
2020-03-06 20:20:11 -0500

    basicShell now can use execvp
    tested with a simple add function i made that needs augments passed to it not fully tested but was successful over multiple calls
    - should test with other programs that take more/different arguments
    - think about how order of execution will work when programs are invoked with <,>,>> and |

--- basicShell.c
+++ basicShell.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <sys/wait.h> 
 
 //------------------------------------------------ Functions will not be added as a library as I need to make a project and I'm not sure what to do their
 
@@ -124,7 +125,7 @@ fgets(buffer,100,stdin); // get command
 //parse command into its individual components
 char* command = strtok(buffer," "); // break command down
 int j=0;
-
+int args = -1; //account initial program call
 while(command != NULL){ // while we still have commands
     //printf("%s\n",command); // print so i can see it happen step by step for testing
     evaluation[j]= command;// fill evaluation arry with each token
@@ -136,9 +137,8 @@ evaluation[j-1] = strtok(evaluation[j-1],"\n");// gets rid of \n from last comma
 //------------------------------------------------------------------------------------------
 //evaluate whats needs to be done and do it
 for(int i=0; i<30;){
-printf("%d-%s\n",i,evaluation[i]); // so i can see what args are in what index
 
-    if(strcmp(evaluation[i],builtIn[0])==0){
+    if(strcmp(evaluation[0],builtIn[0])==0){
 
             char *space[15] = {NULL};
            for(int j=i+1;j<30;){ // this sections accounts for spaces that might be in the path name doesn't account for 2 spaces
@@ -149,19 +149,18 @@ printf("%d-%s\n",i,evaluation[i]); // so i can see what args are in what index
                 strcat(space," ");
                 j=j+1;
             }
-printf("passing %s\n",space); // problem here
         cd(space);
     }
-    else if(strcmp(evaluation[i],builtIn[1])==0){
+    else if(strcmp(evaluation[0],builtIn[1])==0){
         clr();
     }
-    else if(strcmp(evaluation[i],builtIn[2])==0){
+    else if(strcmp(evaluation[0],builtIn[2])==0){
         dir();
     }
-    else if(strcmp(evaluation[i],builtIn[3])==0){
+    else if(strcmp(evaluation[0],builtIn[3])==0){
             envi(envp);
     }
-    else if(strcmp(evaluation[i],builtIn[4])==0){
+    else if(strcmp(evaluation[0],builtIn[4])==0){
                         char echoo[15]="";
            for(int j=i+1;j<30;){ // this sections accounts for spaces that might be in the path name doesn't account for 2 spaces
                 strcat(echoo,evaluation[j]);
@@ -173,35 +172,104 @@ printf("passing %s\n",space); // problem here
             }
         echo(echoo);
     }
-    else if(strcmp(evaluation[i],builtIn[5])==0){
+    else if(strcmp(evaluation[0],builtIn[5])==0){
         help();
     }
-    else if(strcmp(evaluation[i],builtIn[6])==0){
+    else if(strcmp(evaluation[0],builtIn[6])==0){
         pausee();
     }
+	else if(strcmp(evaluation[0],builtIn[7])==0){
+		return 0;
+	}
+
+//-------------------------------------------------------------------------------------- end of built in functions
     else if(strcmp(evaluation[i],"<") == 0){
         printf("%s will replace stdin with %s\n",evaluation[0],evaluation[i+1]);
+        args = args - 1; // acoount for my arg counter count everything that isn't a token
+
+//fd[2]
+//pipe(fd)
+//fd[1] = open (file,O_RDONLY); // assume user has created the file they want to use
+//fork();
+//in child
+//close (0) // childs stdfile stream
+//dup2(fd[1],0)
+//exec
+
+
+
+
     }
     else if(strcmp(evaluation[i],">")==0){
         printf("%s will replace stdout and overwrite %s\n",evaluation[0],evaluation[i+1]);
+        args = args - 1;
     }
     else if(strcmp(evaluation[i],">>")==0){
         printf("%s will append output to %s\n",evaluation[0],evaluation[i+1]);
+        args = args - 1;
     }
     else if(strcmp(evaluation[i],"|")==0){
         printf("%s will write to pipe and %s will read from the pipe\n",evaluation[0],evaluation[i+1]);
+        args = args - 1;
     }
     else if(strcmp(evaluation[i],"&")==0){
         printf("%s will run in the background\n",evaluation[0]);
+        args = args - 1;
+    } else {
+        args = args +1;
     }
-    if(evaluation[i+1]== NULL){
+
+    if(evaluation[i+1]== NULL || strcmp(evaluation[i+1],"\0") == 0){
         break;
     }
-        i=i+1;
-    }
-if(strcmp(evaluation[0],builtIn[7])==0){
-break;
-} // the below statment I suspect was clearing quit before my shell loop could assessl it
+
+    i=i+1;
+}
+
+for(int see =0; see<30;){
+   printf("%d-%s\n",see,evaluation[see]);
+
+see = see +1;
+}
+
+// break down down token index's marked args counted <,>,>>,| (& can be done in above statements)
+// create child to do work
+char* argsPassed [10]={NULL};
+if(args > 0){ // if arguments need to be passed
+argsPassed[0]= evaluation[0];
+for (int i=1; i <=args;){
+    argsPassed[i] = evaluation[i];
+    printf("arg %d - %s\n",i,argsPassed[i]);
+    i=i+1;
+
+}
+}
+int child = fork();
+if(child == 0){
+    execvp(argsPassed[0], argsPassed);
+	printf("didnt execvp\n");
+
+} else {
+
+wait(NULL);
+
+}
+
+
+
+
+
+
+/*
+for(int see =0; see<30;){
+   printf("%d-%s\n",see,evaluation[see]);
+
+see = see +1;
+}
+printf("number of args = %d\n",args);
+
+*/
+//------------------------------------------------------------------------
 for(int i=0;i<30;){ // This loop clears my evaluation buffer
 memset(&evaluation[i],'\0',1); // so privous commands do not interfere
 i=i+1;
@@ -212,3 +280,15 @@ printf("\n"); // just to make it clearer
 return 0;
 }
 
+//piping
+     // 0 = stdin, 1 = stdout
+        //int fd[2];
+        //if(pipe(fd)<0){ // 0 on success, -1 on error
+          //  return -1;
+        //}
+        // fd[1] = open (file,RDONLY) i assume the user has already created the file they wish to use
+        //fork();
+        // close (0);
+        //dup2(fd[1],0) // i want the file to output to program
+        //close(fd[0]) //close out side of pipe
+        //execlp(program,)
